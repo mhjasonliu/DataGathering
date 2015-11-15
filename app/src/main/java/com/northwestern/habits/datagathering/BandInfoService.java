d2eda94467cd69b01d26be49683f069243014d9c
William <williamstogin2018@u.northwestern.edu>
2015-11-15 16:04:37 -0600

    Added service file

---
+++ app/src/main/java/com/northwestern/habits/datagathering/BandInfoService.java
@@ -0,0 +1,16 @@
+package com.northwestern.habits.datagathering;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BandInfoService extends Service {
+    public BandInfoService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
