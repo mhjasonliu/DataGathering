2d2c197f9c9f5c70e64d236bad033d50e55c6a1f
William <williamstogin2018@u.northwestern.edu>
2015-12-16 13:21:05 -0600

    Added quality to heart rate data storage contract

--- app/src/main/java/com/northwestern/habits/datagathering/DataStorageContract.java
+++ app/src/main/java/com/northwestern/habits/datagathering/DataStorageContract.java
@@ -180,11 +180,13 @@ public final class DataStorageContract {
         public static final String COLUMN_NAME_SENSOR_ID = "sensor_id";
         public static final String COLUMN_NAME_DATETIME = "date";
         public static final String COLUMN_NAME_HEART_RATE = "heart_rate";
+        public static final String COLUMN_NAME_QUALITY = "quality";
         private static final String SQL_CREATE_ENTRIES =
                 "CREATE TABLE " + TABLE_NAME + " (" +
                         _ID + " INTEGER PRIMARY KEY" + COMMA_SEP +
                         COLUMN_NAME_SENSOR_ID + INT_TYPE + COMMA_SEP +
                         COLUMN_NAME_DATETIME + DATETIME_TYPE + COMMA_SEP +
+                        COLUMN_NAME_QUALITY + TEXT_TYPE + COMMA_SEP +
                         COLUMN_NAME_HEART_RATE + INT_TYPE +
                         " )";
         private static final String SQL_DELETE_ENTRIES =
