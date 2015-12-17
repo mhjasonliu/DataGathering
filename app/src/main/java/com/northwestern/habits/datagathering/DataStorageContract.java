a53714c64ec9f537401184ae37859f1af07f9c68
William <williamstogin2018@u.northwestern.edu>
2015-12-17 07:21:19 -0600

    Fixed database version number

--- app/src/main/java/com/northwestern/habits/datagathering/DataStorageContract.java
+++ app/src/main/java/com/northwestern/habits/datagathering/DataStorageContract.java
@@ -196,7 +196,7 @@ public final class DataStorageContract {
 
     public static class BluetoothDbHelper extends SQLiteOpenHelper {
         // If you change the database schema, you must increment the database version.
-        public static final int DATABASE_VERSION = 5;
+        public static final int DATABASE_VERSION = 6;
         public static final String DATABASE_NAME = "Bluetooth.db";
 
         public BluetoothDbHelper(Context context) {
