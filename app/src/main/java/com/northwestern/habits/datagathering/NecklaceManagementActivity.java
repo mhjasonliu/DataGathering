fe2e1b1d9c5e7882efa103c6624c91b0870b1b5d
William <williamstogin2018@u.northwestern.edu>
2016-01-12 12:13:49 -0600

    Merge necklace

--- app/src/main/java/com/northwestern/habits/datagathering/NecklaceManagementActivity.java
+++ app/src/main/java/com/northwestern/habits/datagathering/NecklaceManagementActivity.java
@@ -9,5 +9,6 @@ public class NecklaceManagementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_necklace_management);
+
     }
 }
