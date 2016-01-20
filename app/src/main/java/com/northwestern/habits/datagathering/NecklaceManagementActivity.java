8b7c81f34cafc25e622169874f1d0f8d9e5944cc
William <williamstogin2018@u.northwestern.edu>
2016-01-20 12:13:11 -0600

    device is passed to the necklacemanagementactivity

--- app/src/main/java/com/northwestern/habits/datagathering/NecklaceManagementActivity.java
+++ app/src/main/java/com/northwestern/habits/datagathering/NecklaceManagementActivity.java
@@ -1,8 +1,16 @@
 package com.northwestern.habits.datagathering;
 
 import android.bluetooth.BluetoothDevice;
-import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothProfile;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.List;
 
 public class NecklaceManagementActivity extends AppCompatActivity {
 
@@ -11,6 +19,8 @@ public class NecklaceManagementActivity extends AppCompatActivity {
     public static final String NAME_EXTRA = "name";
     public static final String MAC_EXTRA = "mac";
     public static final String DEV_EXTRA = "device";
+
+    private BluetoothGatt mGatt;
 
     private BluetoothDevice device;
 
@@ -23,6 +33,48 @@ public class NecklaceManagementActivity extends AppCompatActivity {
 
         if (extras != null) {
             device = (BluetoothDevice) extras.get(DEV_EXTRA);
+
+            if (device != null) {
+                mGatt = device.connectGatt(this, false, gattCallback);
+            }
         }
     }
+
+    private final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            Log.i("onConnectionStateChange", "Status: " + status);
+            switch (newState) {
+                case BluetoothProfile.STATE_CONNECTED:
+                    Log.i("gattCallback", "STATE_CONNECTED");
+                    gatt.discoverServices();
+                    break;
+                case BluetoothProfile.STATE_DISCONNECTED:
+                    Log.e("gattCallback", "STATE_DISCONNECTED");
+                    // TODO provide user some warning
+                    break;
+                default:
+                    Log.e("gattCallback", "STATE_OTHER");
+            }
+
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            List<BluetoothGattService> services = gatt.getServices();
+            Log.i("onServicesDiscovered", services.toString());
+            gatt.readCharacteristic(services.get(1).getCharacteristics().get
+                    (0));
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt,
+                                         BluetoothGattCharacteristic
+                                                 characteristic, int status) {
+            Log.i("onCharacteristicRead", characteristic.toString());
+            gatt.disconnect();
+        }
+    };
+
+
 }
