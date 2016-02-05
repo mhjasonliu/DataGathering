a95a63b7381841d9d8ab2a8d5b0c9c2ec9648a28
William <williamstogin2018@u.northwestern.edu>
2016-02-05 15:36:16 -0600

    progress...?

--- app/src/main/java/com/northwestern/habits/datagathering/ServerCommunicationService.java
+++ app/src/main/java/com/northwestern/habits/datagathering/ServerCommunicationService.java
@@ -6,7 +6,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.IBinder;
-import android.os.NetworkOnMainThreadException;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -18,7 +17,6 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 public class ServerCommunicationService extends Service {
     public ServerCommunicationService() {
@@ -38,12 +36,12 @@ public class ServerCommunicationService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.v(TAG, "Started service");
         db = (new DataStorageContract.BluetoothDbHelper(getApplicationContext())).getWritableDatabase();
-        if(db != null) {
+        if (db != null) {
             Log.v(TAG, "DB not null");
             Cursor c = db.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
-            Log.v(TAG, "Cursor has" + c.getCount());
+            Log.v(TAG, "Cursor has " + c.getCount() + " tables");
             new SendTableTask().execute(STUDY);
-
+            new SendTableTask().execute(DEVICE);
         }
         //new SendTableTask().execute();
         return Service.START_STICKY;
@@ -66,88 +64,69 @@ public class ServerCommunicationService extends Service {
             switch (tableName) {
                 case STUDY:
                     // Send study table
+                    Cursor c = db.rawQuery("SELECT * FROM " +
+                            DataStorageContract.StudyTable.TABLE_NAME, null);
+                    Log.v(TAG, "Study table returns " + Integer.toString(c.getCount()));
+                    c.moveToFirst();
+                    Log.v(TAG, "Selected and got cursor with " + c.getCount());
+                    int id_col = c.getColumnIndex(DataStorageContract.StudyTable._ID);
+                    int name_col = c.getColumnIndex(DataStorageContract.StudyTable.COLUMN_NAME_STUDY_ID);
+
                     try {
-
-                        Cursor c = db.rawQuery("SELECT * FROM " +
-                                DataStorageContract.StudyTable.TABLE_NAME, null);
-                        c.moveToFirst();
-                        Log.v(TAG, "Selected and got cursor with " + c.getCount());
-                        int id_col = c.getColumnIndex(DataStorageContract.StudyTable._ID);
-                        int name_col = c.getColumnIndex(DataStorageContract.StudyTable.COLUMN_NAME_STUDY_ID);
-
-                        java.net.URL url = new URL(urlBase + "postStudy.py");
+                        URL url = new URL(urlBase + "postStudy.py");
 
                         while (!c.isAfterLast()) {
-                            Log.v(TAG, "Iterating in while loop");
-                            Log.v(TAG, "SEnding post.....");
-                            try {
 
-                                HashMap<String, String> sparams = new HashMap<>();
-                                sparams.put("id", Integer.toString(c.getInt(id_col)));
-                                sparams.put("name", c.getString(name_col));
+                            HashMap<String, String> sparams = new HashMap<>();
+                            sparams.put("id", Integer.toString(c.getInt(id_col)));
+                            sparams.put("name", c.getString(name_col));
+                            Log.v(TAG, "Sending study " + c.getString(name_col));
+                            sendBytes(sparams, url);
 
-                                Log.v(TAG, "Put sparams");
-
-                                Set set = sparams.entrySet();
-                                StringBuilder postData = new StringBuilder();
-                                for (Map.Entry<String, String> param : sparams.entrySet()) {
-                                    if (postData.length() != 0) {
-                                        postData.append('&');
-                                    }
-                                    postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
-                                    postData.append('=');
-                                    postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
-                                }
-
-                                byte[] postDataBytes = postData.toString().getBytes("UTF-8");
-
-                                Log.v(TAG, "Encoded post data");
-
-                                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                                conn.setRequestMethod("POST");
-                                conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
-                                conn.setDoOutput(true);
-                                Log.v(TAG, "Bytes are: " + new String(postDataBytes));
-                                conn.getOutputStream().write(postDataBytes);
-
-                                Log.v(TAG, "Posted data");
-
-                                Log.v(TAG, "Response code " + conn.getResponseMessage());
-
-                                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-                                StringBuilder builder = new StringBuilder();
-                                Log.v(TAG, "Reading response");
-                                for (String line = null; (line = reader.readLine()) != null;) {
-                                    builder.append(line).append("\n");
-                                }
-
-                                reader.close();
-                                conn.disconnect();
-                                Log.v(TAG, "Disconnected");
-                                Log.v(TAG, "Response: " + builder.toString());
-
-                            } catch (MalformedURLException e) {
-                                e.printStackTrace();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
                             c.moveToNext();
                         }
-                        c.close();
                     } catch (MalformedURLException e) {
                         e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (NetworkOnMainThreadException e) {
-                        e.printStackTrace();
-                    } catch (Exception e) {
+                    }
+                    break;
+                case DEVICE:
+                    // Send device table
+                    c = db.rawQuery("SELECT * FROM " +
+                            DataStorageContract.DeviceTable.TABLE_NAME, null);
+                    c.moveToFirst();
+                    id_col = c.getColumnIndex(DataStorageContract.DeviceTable._ID);
+                    int study_col = c.getColumnIndex(DataStorageContract.DeviceTable.COLUMN_NAME_STUDY_ID);
+                    int type_col = c.getColumnIndex(DataStorageContract.DeviceTable.COLUMN_NAME_TYPE);
+                    int mac_col = c.getColumnIndex(DataStorageContract.DeviceTable.COLUMN_NAME_MAC);
+                    int location_col = c.getColumnIndex(DataStorageContract.DeviceTable.COLUMN_NAME_LOCATION);
+
+                    Log.v(TAG, "REquest is " + Integer.toString(c.getInt(id_col)) +
+                    c.getInt(study_col) +
+                    c.getString(type_col) +
+                    c.getString(mac_col) +
+                    c.getString(location_col));
+
+
+                    try {
+                        URL url = new URL(urlBase + "postDevice.py");
+
+                        while (!c.isAfterLast()) {
+                            HashMap<String, String> sparams = new HashMap<>();
+                            sparams.put("id", Integer.toString(c.getInt(id_col)));
+                            sparams.put("study_id", c.getString(study_col));
+                            sparams.put("type", c.getString(type_col));
+                            sparams.put("mac_address", c.getString(mac_col));
+                            sparams.put("location", c.getString(location_col));
+
+                            Log.v(TAG, "Sending device " + c.getString(type_col));
+                            sendBytes(sparams, url);
+
+                            c.moveToNext();
+                        }
+                    } catch (MalformedURLException e) {
                         e.printStackTrace();
                     }
 
-
-                    break;
-                case DEVICE:
-                    // Send Device table
 
                     break;
                 case SENSOR:
@@ -169,8 +148,49 @@ public class ServerCommunicationService extends Service {
     }
 
 
-    private void sendTable(Cursor c) {
+    private void sendBytes(HashMap<String, String> sparams, URL url) {
 
+        HttpURLConnection conn = null;
+
+        try {
+
+            StringBuilder postData = new StringBuilder();
+            for (Map.Entry<String, String> param : sparams.entrySet()) {
+                if (postData.length() != 0) {
+                    postData.append('&');
+                    postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+                    postData.append('=');
+                    postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+                }
+            }
+
+            byte[] postDataBytes;
+            postDataBytes = postData.toString().getBytes("UTF-8");
+
+            conn = null;
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+
+
+            conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+            conn.setDoOutput(true);
+            Log.v(TAG, "*********************************" + postData.toString());
+            conn.getOutputStream().write(postDataBytes);
+
+            BufferedReader reader = null;
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            StringBuilder builder = new StringBuilder();
+            for (String line = null; (line = reader.readLine()) != null; ) {
+                builder.append(line).append("\n");
+            }
+            Log.v(TAG, "Response: " + builder.toString());
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null)
+                conn.disconnect();
+        }
 
     }
 }
