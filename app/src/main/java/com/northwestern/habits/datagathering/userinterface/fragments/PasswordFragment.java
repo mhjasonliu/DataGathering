e2fc6b73ca8f92eaac4f4e7fa77ed916a26981c4
William <williamstogin2018@u.northwestern.edu>
2016-06-17 22:25:00 -0500

    Improved layout of passwordfragment

--- app/src/main/java/com/northwestern/habits/datagathering/userinterface/fragments/PasswordFragment.java
+++ app/src/main/java/com/northwestern/habits/datagathering/userinterface/fragments/PasswordFragment.java
@@ -249,7 +249,7 @@ public class PasswordFragment extends Fragment {
         }
     };
 
-    private View.OnClickListener finishClickListener = new View.OnClickListener() {;;;;;;;;;;;;;;;;;
+    private View.OnClickListener finishClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             getActivity().onBackPressed();
