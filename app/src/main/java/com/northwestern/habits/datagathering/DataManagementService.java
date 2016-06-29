0e32a8f23d7503a1aac298e7721e04acd6c2492e
William <williamstogin2018@u.northwestern.edu>
2016-06-29 08:06:08 -0500

    Added constant strings for band stream types

--- app/src/main/java/com/northwestern/habits/datagathering/DataManagementService.java
+++ app/src/main/java/com/northwestern/habits/datagathering/DataManagementService.java
@@ -18,6 +18,20 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class DataManagementService extends Service {
 
+    public static final String T_ACCEL = "Accelerometer";
+    public static final String T_Altimeter = "Altimeter";
+    public static final String T_Ambient = "Ambient_Light";
+    public static final String T_Barometer = "Barometer";
+    public static final String T_Calories = "Calories";
+    public static final String T_Distance = "Distance";
+    public static final String T_GSR = "GSR";
+    public static final String T_Gyroscope = "Gyroscope";
+    public static final String T_Heart_Rate = "Heart_Rate";
+    public static final String T_PEDOMETER = "Pedometer";
+    public static final String T_SKIN_TEMP = "Skin_Temperature";
+    public static final String T_UV = "UV";
+
+
     public interface DataManagementFunctions {
         void placeHolder(String text);
     }
