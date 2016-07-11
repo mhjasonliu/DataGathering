dcf8b87a7dc3b4eb0c8a3dee683ad9afdd0a0b3e
William <williamstogin2018@u.northwestern.edu>
2016-07-11 11:21:30 -0500

    Removed unused bloat from the datagatheringapplication class

--- app/src/main/java/com/northwestern/habits/datagathering/DataGatheringApplication.java
+++ app/src/main/java/com/northwestern/habits/datagathering/DataGatheringApplication.java
@@ -1,6 +1,5 @@
 package com.northwestern.habits.datagathering;
 
-import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
@@ -17,25 +16,16 @@ import java.util.Calendar;
  */
 public class DataGatheringApplication extends Application implements Thread.UncaughtExceptionHandler {
     private static DataGatheringApplication ourInstance = new DataGatheringApplication();
-//    private Manager manager;
-//    private Database db;
     private static String TAG = "Application";
 
     public static DataGatheringApplication getInstance() {
         return ourInstance;
     }
 
-//    public Database getDatabase() {
-//        return db;
-//    }
-
     @Override
     public void onCreate() {
         super.onCreate();
         ourInstance = this;
-//        Thread.setDefaultUncaughtExceptionHandler(this);
-
-//        createDatabase(getApplicationContext());
         // If we do not currently have a document, create one and save the id
         SharedPreferences prefs = getSharedPreferences(Preferences.NAME, MODE_PRIVATE);
         String id = prefs.getString(Preferences.CURRENT_DOCUMENT, "");
@@ -50,15 +40,6 @@ public class DataGatheringApplication extends Application implements Thread.Unca
             }
             CouchBaseData.setCurrentDocument(id);
         }
-
-        // Start timertask that moves the folders
-//        Calendar c = Calendar.getInstance();
-//        c.add(Calendar.HOUR_OF_DAY, 1);
-//        c.add(Calendar.MINUTE, 30);
-//        Timer t = new Timer();
-//        t.scheduleAtFixedRate(new FileManagerTimer(this.getApplicationContext()),
-//                c.getTime(), TimeUnit.HOURS.toMillis(1) - 1000); // Slightly less than one hour to
-                // avoid missing an hour time
         }
 
     @Override
@@ -119,30 +100,5 @@ public class DataGatheringApplication extends Application implements Thread.Unca
         c.setTimeInMillis(timestamp);
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         return format.format(c.getTime());
-    }
-
-//    private void createDatabase(Context context) {
-//        try {
-//            this.manager = new Manager(new AndroidContext(context), Manager.DEFAULT_OPTIONS);
-//            this.db = manager.getDatabase("data_gathering_db");
-//            db.setMaxRevTreeDepth(1);
-//        } catch (IOException e) {
-//            Log.e(TAG, "Cannot create database", e);
-//        } catch (CouchbaseLiteException e1) {
-//            e1.printStackTrace();
-//        }
-//    }
-
-//    private Document currentDocument;
-//    public Document getCurrentDocument() { return currentDocument; }
-
-
-    /* ********************************* MANAGE THE SERVICE ********************************** */
-    public void registerActivity(Activity activity) {
-
-    }
-
-    void unregisterActivity(Activity activity) {
-
     }
 }
