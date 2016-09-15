87f250b18c2f3b7ed1664a71399b2b86a897b4e1
William <williamstogin2018@u.northwestern.edu>
2016-09-15 11:35:01 -0500

    Added functionality for adding the label to the database

--- app/src/main/java/com/northwestern/habits/datagathering/database/LabelManager.java
+++ app/src/main/java/com/northwestern/habits/datagathering/database/LabelManager.java
@@ -1,15 +1,43 @@
 package com.northwestern.habits.datagathering.database;
 
+import android.content.Context;
+
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Document;
+import com.couchbase.lite.UnsavedRevision;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by William on 9/15/2016
  */
 public class LabelManager {
 
-    public static void addLabelChange() {
+    public static void addLabelChange(String userID, Context c, final String label, final long timeStamp) {
         // Add change to the csv
 
-        // Add change to the database
 
+        // Add change to the database
+        try {
+            Document d = CouchBaseData.getLabelDocument(userID, c);
+            d.update(new Document.DocumentUpdater() {
+                @Override
+                public boolean update(UnsavedRevision newRevision) {
+                    Map<String, Object> properties = newRevision.getProperties();
+                    Map<Long, String> labels = (Map<Long, String>) properties.get("labels");
+                    if (labels == null) {
+                        labels = new HashMap();
+                        properties.put("labels", labels);
+                    }
+                    labels.put(timeStamp,label);
+                    return true;
+                }
+            });
+        } catch (CouchbaseLiteException | IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
