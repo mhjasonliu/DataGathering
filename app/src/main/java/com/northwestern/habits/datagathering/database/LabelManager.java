481b896429a3cc1bd0e50eb715571f55182c66e6
William <williamstogin2018@u.northwestern.edu>
2016-09-15 16:08:21 -0500

    Added label csv writing

--- app/src/main/java/com/northwestern/habits/datagathering/database/LabelManager.java
+++ app/src/main/java/com/northwestern/habits/datagathering/database/LabelManager.java
@@ -7,7 +7,10 @@ import com.couchbase.lite.CouchbaseLiteException;
 import com.couchbase.lite.Document;
 import com.couchbase.lite.UnsavedRevision;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -20,7 +23,37 @@ public class LabelManager {
     private static final String TAG = "LabelManager";
 
     public static void addLabelChange(String userID, Context c, final String label, final long timeStamp) {
+        Map<String, Object> dataMap = new HashMap<>();
+        dataMap.put("Time", timeStamp);
+        dataMap.put("Label", label);
+        final List<Map<String, Object>> dataList = new LinkedList<>();
+        dataList.add(dataMap);
+        List<String> properties = Arrays.asList("Time", "Label");
+
         // Add change to the csv
+        File folder = CsvWriter.getFolder(timeStamp, userID, "Label");
+        File csv = CsvWriter.getCsv(folder, timeStamp);
+        FileWriter writer = null;
+        try {
+            if (!csv.exists()) {
+                writer = CsvWriter.writeProperties(properties, csv, c);
+            } else {
+                writer = new FileWriter(csv, true);
+            }
+
+            CsvWriter.writeDataSeries(writer, dataList, properties);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.flush();
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
 
         // Add change to the database
@@ -30,15 +63,14 @@ public class LabelManager {
                 @Override
                 public boolean update(UnsavedRevision newRevision) {
                     Map<String, Object> properties = newRevision.getProperties();
-                    List<Map<String, Object>> labels = (List<Map<String, Object>>) properties.get(DataManagementService.DATA);
+                    List<Map<String, Object>> labels =
+                            (List<Map<String, Object>>) properties.get(DataManagementService.DATA);
                     if (labels == null) {
-                        labels = new LinkedList<>();
-                        properties.put(DataManagementService.DATA, labels);
+                        properties.put(DataManagementService.DATA, dataList);
+                    } else {
+                        ((List<Map<String, Object>>) properties.get(DataManagementService.DATA))
+                                .addAll(dataList);
                     }
-                    Map newMap = new HashMap<>();
-                    newMap.put("Time", timeStamp);
-                    newMap.put("Label", label);
-                    labels.add(newMap);
                     Log.v(TAG, "Updated database");
                     return true;
                 }
@@ -48,8 +80,6 @@ public class LabelManager {
         }
 
     }
-
-
 
 
 }
