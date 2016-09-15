7edc69564998cf7dd5c83f6e9acb1e31e8782667
William <williamstogin2018@u.northwestern.edu>
2016-09-15 11:14:48 -0500

    Started new label class to extract it from band stuff

---
+++ app/src/main/java/com/northwestern/habits/datagathering/database/LabelManager.java
@@ -0,0 +1,19 @@
+package com.northwestern.habits.datagathering.database;
+
+/**
+ * Created by William on 9/15/2016
+ */
+public class LabelManager {
+
+    public static void addLabelChange() {
+        // Add change to the csv
+
+        // Add change to the database
+
+
+    }
+
+
+
+
+}
