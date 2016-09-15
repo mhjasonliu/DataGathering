28022cf0d5a1bccf56c559b91d081adde9072f46
William <williamstogin2018@u.northwestern.edu>
2016-09-15 15:53:24 -0500

    Moved meat of writing csv from DataSeris to CsvWriter class

--- app/src/main/java/com/northwestern/habits/datagathering/banddata/DataSeries.java
+++ app/src/main/java/com/northwestern/habits/datagathering/banddata/DataSeries.java
@@ -2,16 +2,12 @@ package com.northwestern.habits.datagathering.banddata;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Environment;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
-import com.microsoft.band.sensors.HeartRateQuality;
-import com.microsoft.band.sensors.MotionType;
+import com.northwestern.habits.datagathering.database.CsvWriter;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -22,7 +18,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Created by William on 8/2/2016
@@ -31,7 +26,7 @@ public class DataSeries {
 
     private int capacity = 0;
 
-    private List<Map> dataArray = new LinkedList<>();
+    private List<Map<String, Object>> dataArray = new LinkedList<>();
     private long firstEntry;
     private long lastEntry;
     private String type;
@@ -79,8 +74,6 @@ public class DataSeries {
         return split;
     }
 
-    public Set getDataKeys() {return dataArray.get(0).keySet();}
-
     public int getCount() {
         return dataArray.size();
     }
@@ -108,131 +101,38 @@ public class DataSeries {
                 Log.e(TAG, "permission denied");
             }
 
-            // Date for use in the folder path
-            Calendar date = Calendar.getInstance();
-            date.setTimeInMillis(firstEntry);
-            int day = date.get(Calendar.DAY_OF_MONTH);
-            int month = date.get(Calendar.MONTH) + 1;
-            int year = date.get(Calendar.YEAR);
-            int hour = date.get(Calendar.HOUR_OF_DAY);
-            String hourst = (hour < 10)
-                    ? "0" + Integer.toString(hour) + "00"
-                    : Integer.toString(hour) + "00";
-            int minute = date.get(Calendar.MINUTE);
-            String dateString = Integer.toString(month) + "-"
-                    + Integer.toString(day) + "-"
-                    + Integer.toString(year);
+            File folder = CsvWriter.getFolder(firstEntry, userID, type);
 
-            String PATH = Environment.getExternalStorageDirectory() + "/Bandv2/" +
-                    userID + "/" + type + "/" + dateString + "/" + hourst;
-            File folder = new File(PATH);
-            if (!folder.exists()) {
-                Log.v(TAG, "directory " + folder.getPath() + " Succeeded " + folder.mkdirs());
-            }
+            // Make csv
+            File csv = CsvWriter.getCsv(folder, firstEntry);
 
-            // Make csv name
-            hourst = Integer.toString(hour);
-            String fName = hourst.concat(":" + Integer.toString(minute) + ".csv");
-
-            File csv = new File(PATH, fName);
-            List<Map> dataSeries = dataArray;
+            List<Map<String, Object>> dataSeries = dataArray;
             List<String> properties = new LinkedList(dataArray.get(0).keySet());
+            FileWriter writer = null;
             try {
-                FileWriter csvWriter = null;
-                try {
-                csvWriter = writeProperties(properties, csv);
-                    // Write the file
-                    for (Map<String, Object> dataPoint :
-                            dataSeries) {
-                        for (int i = 0; i < properties.size(); i++) {
-                            long timestamp = Long.valueOf((String) dataPoint.get("Time"));
-                            date.setTimeInMillis(timestamp);
-                            if (minute != date.get(Calendar.MINUTE)) {
-                                minute = date.get(Calendar.MINUTE);
-                                fName = hourst.concat(":" + Integer.toString(minute) + ".csv");
-                                Log.v(TAG, fName);
-                                csv = new File(PATH, fName);
-                                csvWriter.flush();
-                                csvWriter.close();
-                                csvWriter = writeProperties(properties, csv);
-                            }
+                if (!csv.exists()) {
+                    writer = CsvWriter.writeProperties(properties, csv, context);
+                } else {
+                    writer = new FileWriter(csv, true);
+                }
 
+                CsvWriter.writeDataSeries(writer, dataSeries, properties);
+                Log.v(TAG, "Wrote the file");
 
-                            try {
-                                Object datum = dataPoint.get(properties.get(i));
-
-                                if (datum instanceof String) {
-                                    csvWriter.append(datum.toString());
-                                } else if (datum instanceof Double) {
-                                    csvWriter.append(Double.toString((Double) datum));
-                                } else if (datum instanceof Integer) {
-                                    csvWriter.append(Integer.toString((Integer) datum));
-                                } else if (datum instanceof Long) {
-                                    csvWriter.append(Long.toString((Long) datum));
-                                } else if (datum instanceof Float) {
-                                    csvWriter.append(Float.toString((Float) datum));
-                                } else if (datum instanceof HeartRateQuality) {
-                                    csvWriter.append(datum.toString());
-                                } else if (datum instanceof MotionType) {
-                                    csvWriter.append(datum.toString());
-                                } else {
-                                    Log.e(TAG, "Unhandled case " + datum.getClass());
-                                    csvWriter.append(datum.toString());
-                                }
-                                if (i == properties.size() - 1) {
-                                    csvWriter.append("\n");
-                                } else {
-                                    csvWriter.append(",");
-                                }
-                            } catch (NullPointerException e) {
-                                Log.e(TAG, "Row was null");
-                                e.printStackTrace();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                    Log.v(TAG, "Wrote the file");
-
-                } catch (ConcurrentModificationException | IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    if (csvWriter != null) {
-                        csvWriter.flush();
-                        csvWriter.close();
+            } catch (ConcurrentModificationException | IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (writer != null) {
+                    try {
+                        writer.flush();
+                        writer.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
 
             return null;
-        }
-
-        private FileWriter writeProperties(List<String> properties, File csv) throws IOException {
-            if (!csv.exists()) {
-                // Make the file
-                if (!csv.createNewFile()) {
-                    throw new IOException();
-                }
-                Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-                intent.setData(Uri.fromFile(csv));
-                context.sendBroadcast(intent);
-
-
-                FileWriter csvWriter = new FileWriter(csv.getPath(), true);
-                for (int i = 0; i < properties.size(); i++) {
-                    csvWriter.append(properties.get(i));
-                    if (i == properties.size() - 1) {
-                        csvWriter.append("\n");
-                    } else {
-                        csvWriter.append(",");
-                    }
-                }
-                return csvWriter;
-            } else {
-                return new FileWriter(csv.getPath(), true);
-            }
         }
     }
 }
--- app/src/main/java/com/northwestern/habits/datagathering/database/CsvWriter.java
+++ app/src/main/java/com/northwestern/habits/datagathering/database/CsvWriter.java
@@ -1,10 +1,17 @@
 package com.northwestern.habits.datagathering.database;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Environment;
 import android.util.Log;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by William on 9/15/2016
@@ -12,6 +19,12 @@ import java.util.Calendar;
 public class CsvWriter {
     private static final String TAG = "CsvWriter";
 
+    /** Decides the name of the folder from parameters given. Creates the folder if non-existant
+     * @param timestamp the timestamp of a datum that will be in the folder
+     * @param userID The ID of the user associated with this data
+     * @param type The type of data to be contained int he folder
+     * @return File with the path of the folder
+     */
     public static File getFolder(long timestamp, String userID, String type) {
         // Date for use in the folder path
         Calendar date = Calendar.getInstance();
@@ -34,6 +47,81 @@ public class CsvWriter {
             Log.v(TAG, "directory " + folder.getPath() + " Succeeded " + folder.mkdirs());
         }
         return folder;
+    }
+
+    /** Creates the csv file in the folder
+     * @param folder the folder in which the csv file should reside
+     * @param timestamp timestamp of a datum to be contained in the csv
+     * @return the file representing the csv
+     */
+    public static File getCsv(File folder, long timestamp) {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(timestamp);
+        String hourst = Integer.toString(c.get(Calendar.HOUR_OF_DAY));
+        int minute = c.get(Calendar.MINUTE);
+        String fName = hourst.concat(":" + Integer.toString(minute) + ".csv");
+        return new File(folder.getPath(), fName);
+    }
+
+    /** Fills out the header of the csv from the given properties
+     * @param properties List of csv headers
+     * @param csv File representing the csv
+     * @param context
+     * @return FileWriter for the csv CLOSE TO AVOID MEMORY LEAK
+     * @throws IOException
+     */
+    public static FileWriter writeProperties(List<String> properties, File csv, Context context)
+            throws IOException {
+        if (!csv.exists()) {
+            // Make the file
+            if (!csv.createNewFile()) {
+                throw new IOException();
+            }
+            Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            intent.setData(Uri.fromFile(csv));
+            context.sendBroadcast(intent);
+
+
+            FileWriter csvWriter = new FileWriter(csv.getPath(), true);
+            for (int i = 0; i < properties.size(); i++) {
+                csvWriter.append(properties.get(i));
+                if (i == properties.size() - 1) {
+                    csvWriter.append("\n");
+                } else {
+                    csvWriter.append(",");
+                }
+            }
+            return csvWriter;
+        } else {
+            return new FileWriter(csv.getPath(), true);
+        }
+    }
+
+    /** Appends the data in dataList to the csvWriter
+     * @param csvWriter Open file writer for writing th csv
+     * @param dataList List of data to be written
+     * @param properties List of headers of the csv (in the order of the csv)
+     */
+    public static void writeDataSeries(FileWriter csvWriter, List<Map<String, Object>> dataList,
+                                       List<String> properties) {
+        boolean newLine = true;
+        for (Map<String, Object> datum :
+                dataList) {
+            for (String property :
+                    properties) {
+                try {
+                    if (!newLine) csvWriter.append(",");
+                    csvWriter.append(datum.get(property).toString());
+                    newLine = false;
+                } catch (IOException e) {e.printStackTrace();}
+            }
+            try {
+                csvWriter.append("\n");
+                newLine = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
