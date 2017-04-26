70c9db79f6c0a4ffd9e9f4858b923889a22c96df
OPTRASYSTEMS\Y.Misal <y.misal@optrasystems.com>
2017-04-26 12:17:55 +0530

    updated code to call periodic data service

--- wear/src/main/java/com/northwestern/habits/datagathering/Receiver.java
+++ wear/src/main/java/com/northwestern/habits/datagathering/Receiver.java
@@ -24,8 +24,9 @@ public class Receiver extends BroadcastReceiver {
 
         ComponentName serviceComponent = new ComponentName(context, WearJobService.class);
         JobInfo.Builder builder = new JobInfo.Builder(0, serviceComponent);
-        builder.setMinimumLatency(1000); // wait at least
-        builder.setOverrideDeadline(5 * 1000); // maximum delay
+        builder.setPeriodic(1000);
+//        builder.setMinimumLatency(1000); // wait at least
+//        builder.setOverrideDeadline(5 * 1000); // maximum delay
         builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED); // require unmetered network
         builder.setRequiresDeviceIdle(true); // device should be idle
         builder.setRequiresCharging(false); // we don't care if the device is charging or not
