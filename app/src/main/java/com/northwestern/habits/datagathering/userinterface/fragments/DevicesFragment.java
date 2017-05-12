f7d29ba9105d0fb49c0b9e89121cbac3735f8dd5
OPTRASYSTEMS\Y.Misal <y.misal@optrasystems.com>
2017-05-12 17:30:34 +0530

    Code refine

--- app/src/main/java/com/northwestern/habits/datagathering/userinterface/fragments/DevicesFragment.java
+++ app/src/main/java/com/northwestern/habits/datagathering/userinterface/fragments/DevicesFragment.java
@@ -248,7 +248,6 @@ public class DevicesFragment extends Fragment {
         }
     }
 
-
         /**
          * This interface must be implemented by activities that contain this
          * fragment to allow an interaction in this fragment to be communicated
