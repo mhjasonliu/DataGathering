b238a55fb3416f88cefe261810c62f9b4dd34d5c
OPTRASYSTEMS\Y.Misal <y.misal@optrasystems.com>
2017-05-12 17:41:11 +0530

    Set alarm on first launch to register a receiver to collect data continuously.

--- wear/src/main/java/com/northwestern/habits/datagathering/MainActivity.java
+++ wear/src/main/java/com/northwestern/habits/datagathering/MainActivity.java
@@ -1,15 +1,25 @@
 package com.northwestern.habits.datagathering;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
 
     private TextView mTextView;
+    private int REQUEST = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +33,26 @@ public class MainActivity extends Activity {
             }
         });
 
-        Intent i = new Intent(this, DataService.class);
-        startService(i);
+        /*Intent i = new Intent(this, DataService.class);
+        startService(i);*/
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.BODY_SENSORS);
+        if (permissionCheck == -1) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.BODY_SENSORS, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST);
+        }
+
+        final PendingIntent localPendingIntent = PendingIntent.getService(this , 0, new Intent(this, DataService.class), 0);
+        final AlarmManager localAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        final Handler handler = new Handler();
+        final Runnable r = new Runnable() {
+            public void run() {
+                localAlarmManager.setRepeating(AlarmManager.RTC_WAKEUP, Calendar.getInstance().getTimeInMillis(), 5000, localPendingIntent);
+            }
+        };
+        handler.postDelayed(r, 100);
 
     }
 }
