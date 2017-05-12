64234c4fe48e512ecefa84fef2b4d5bf1410bcef
OPTRASYSTEMS\Y.Misal <y.misal@optrasystems.com>
2017-05-12 17:32:24 +0530

    Connectivity change receiver added

--- app/src/main/java/com/northwestern/habits/datagathering/userinterface/SplashActivity.java
+++ app/src/main/java/com/northwestern/habits/datagathering/userinterface/SplashActivity.java
@@ -3,6 +3,7 @@ package com.northwestern.habits.datagathering.userinterface;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -13,8 +14,11 @@ import com.northwestern.habits.datagathering.banddata.sensors.BandDataService;
 import com.northwestern.habits.datagathering.database.DataManagementService;
 import com.northwestern.habits.datagathering.weardata.WearDataService;
 
+import java.util.Calendar;
+
 public class SplashActivity extends Activity {
 
+    MyReceiver myReceiver = new MyReceiver();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +31,9 @@ public class SplashActivity extends Activity {
         if (prefs.getString(Preferences.PASSWORD, "").equals("")) {
             i = new Intent(this, AdvancedSettingsActivity.class);
         } else {
+            final IntentFilter mIFNetwork = new IntentFilter();
+            mIFNetwork.addAction(android.net.ConnectivityManager.CONNECTIVITY_ACTION); //"android.net.conn.CONNECTIVITY_CHANGE"
+            registerReceiver(myReceiver, mIFNetwork);
             i = new Intent (this, UserActivity.class);
         }
 
@@ -52,5 +59,17 @@ public class SplashActivity extends Activity {
         // Connect to the wear device
         i = new Intent(context, WearDataService.class);
         context.startService(i);
+    }
+
+    @Override
+    protected void onDestroy() {
+        try{
+            if(myReceiver!=null)
+                unregisterReceiver(myReceiver);
+        }catch(Exception e)
+        {
+
+        }
+        super.onDestroy();
     }
 }
